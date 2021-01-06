297cfc1e67949723c68175b800e7ef6338eb82d4
weixiaoshan@qq.com <weixiaoshan@qq.com>
2021-01-06 10:18:31 +0800

    轮廓图的完成

--- src/pages/index/main.vue
+++ src/pages/index/main.vue
@@ -1,18 +1,13 @@
 <template>
-	<view>
+	<view class="content">
 		<Search></Search>
 		<swiper  class="swiper" :indicator-dots="indicatorDots" 
 		indicator-active-color="#ea4350"   circular="circuar"
 		:autoplay="autoplay" :interval="interval" :duration="duration">
-			<swiper-item>
-				<image src="../../static/cart_selected.png" mode="" />
+			<swiper-item class="swiper-item" v-for="item in swiperData" :key="item.index">
+				<image class="image" :src="item.image_src" mode="aspectFill" />
 			</swiper-item>
-				<swiper-item>
-				<image src="../../static/cart_selected.png" mode="" />
-			</swiper-item>
-			<swiper-item>
-				<image src="../../static/cart_selected.png" mode="" />
-			</swiper-item>
+				
 		</swiper>
 	</view>
 </template>
@@ -23,16 +18,25 @@ import Search from "../../components/search"
 		components:{Search},
 		data() {
 			return {
+				swiperData:[],
 			circular:true,
 		     indicatorDots: true,
             autoplay: true,
-            interval: 2000,
-            duration: 500
+            interval: 3000,
+            duration: 1500
 				
 		
 			}
 		},
 		onLoad() {
+			uni.request({
+				url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata', 
+				success: (res) => {
+					console.log(res.data);
+					this.swiperData=res.data.message
+					// this.text = 'request success';
+				}
+			});
 
 		},
 		methods: {
@@ -41,6 +45,18 @@ import Search from "../../components/search"
 	}
 </script>
 
-<style>
-	
+<style lang="less" scoped>
+.content{
+	margin: 100rpx 0 0 0;
+.swiper {
+	height: 340rpx;
+.swiper-item {
+  .image {
+	height: 340rpx;
+	  width: 100%;
+
+    }
+  }
+}
+}
 </style>
