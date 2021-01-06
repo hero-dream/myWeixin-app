b28689b1df10e7ce0f690874de58250c75435336
weixiaoshan@qq.com <weixiaoshan@qq.com>
2021-01-06 15:54:06 +0800

    分类的容器大体布局

--- src/pages/category/main.vue
+++ src/pages/category/main.vue
@@ -1,13 +1,40 @@
 <template>
-  <view>分类页面</view>
+  <view class="cats">
+    <Search></Search>
+    <view class="category">
+      <scroll-view scroll-y class="category-left">
+        <view>左边</view>
+      </scroll-view>
+      <scroll-view scroll-y class="category-right">
+        <view>右边</view>
+      </scroll-view>
+    </view>
+  </view>
 </template>
 
 <script>
+import Search from "../../components/search";
 export default {
-
-}
+  components: { Search },
+};
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.cats {
+  .category {
+    margin: 90rpx 0 0 0;
+    display: flex;
+    .category-left {
+      flex: 2;
+      height: calc(100vh - 90rpx);
+      background-color: #f3f3f3;
+      color: #909090;
+    }
+    .category-right {
+      flex: 5;
+      height: calc(100vh - 90rpx);
+      background-color: pink;
+    }
+  }
+}
 </style>
