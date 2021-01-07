1027715e406361a9d809a4bd6c921c54580f5d2d
weixiaoshan@qq.com <weixiaoshan@qq.com>
2021-01-07 16:04:12 +0800

    左边的点击切换根据索引进行右边切换和右边的样式布局完成

--- src/pages/category/main.vue
+++ src/pages/category/main.vue
@@ -3,10 +3,35 @@
     <Search></Search>
     <view class="category">
       <scroll-view scroll-y class="category-left">
-        <view class="item-left" :class="isActive===index? 'active': '' " @tap="changeTabs(index)"  v-for="(item,index) in categoryList" :key="item.cat_id">{{item.cat_name}}</view>
+        <view
+          class="item-left"
+          :class="isActive === index ? 'active' : ''"
+          @tap="changeTabs(index)"
+          v-for="(item, index) in categoryList"
+          :key="item.cat_id"
+          >{{ item.cat_name }}</view
+        >
       </scroll-view>
       <scroll-view scroll-y class="category-right">
-        <view class="item-right"></view>
+        <view
+          class="item-right"
+          v-for="item1 in categoryRight"
+          :key="item1.index"
+        >
+          <view class="goods_title">
+            <text class="item-text">/</text>
+            <text> {{ item1.cat_name }}</text>
+            <text class="item-text">/</text>
+          </view>
+          <view
+            class="goods_list">
+            <view class="goods-item"   v-for="item2 in item1.children"
+            :key="item2.cat_id">
+              <image class="goods-img" :src="item2.cat_icon" mode="widthFix" />
+              <text class="text">{{ item2.cat_name }}</text>
+            </view>
+          </view>
+        </view>
       </scroll-view>
     </view>
   </view>
@@ -16,44 +41,39 @@
 import Search from "../../components/search";
 export default {
   components: { Search },
-  data(){
-    return{
-     isActive:0,
-      categoryList:[],
-      // categoryRight:[]
-    }
-   
+  data() {
+    return {
+      isActive: 0,
+      categoryList: [],
+      categoryRight: [],
+      categoryData: [],
+    };
   },
-   onLoad(){
-     this.getCats()
- 
-
-   },
-   methods:{
-     getCats(){
-           uni.request({
-       url: 'https://api-hmugo-web.itheima.net/api/public/v1/categories',
-       success: (res) => {
-         console.log(res.data);
-         const categoryData = res.data.message;
-    this.categoryList = categoryData.map(item=>{
-      return{
-     cat_name:item.cat_name
-      }
-    }
-    
-    )
-    // this.categoryRight =categoryData[0].children
-   
-   
-       }
-     });
-     },
-     changeTabs(index){
-   this.isActive=index
-     }
-     
-   }
+  onLoad() {
+    this.getCats();
+  },
+  methods: {
+    getCats() {
+      uni.request({
+        url: "https://api-hmugo-web.itheima.net/api/public/v1/categories",
+        success: (res) => {
+          console.log(res.data);
+          const categoryData = res.data.message;
+          this.categoryData = categoryData;
+          this.categoryList = categoryData.map((item) => {
+            return {
+              cat_name: item.cat_name,
+            };
+          });
+          this.categoryRight = categoryData[0].children;
+        },
+      });
+    },
+    changeTabs(index) {
+      this.isActive = index;
+      this.categoryRight = this.categoryData[index].children;
+    },
+  },
 };
 </script>
 
@@ -67,12 +87,12 @@ export default {
       height: calc(100vh - 90rpx);
       background-color: #f3f3f3;
       color: #909090;
-      .item-left{
+      .item-left {
         display: flex;
         align-items: center;
         justify-content: center;
         height: 100rpx;
-        &.active{
+        &.active {
           color: red;
           border-left: red solid 5rpx;
         }
@@ -81,7 +101,37 @@ export default {
     .category-right {
       flex: 5;
       height: calc(100vh - 90rpx);
-  
+      .item-right {
+        .goods_title {
+          height: 100rpx;
+          color: #bfbaad;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          .item-text {
+            color: #7b7970;
+          }
+        }
+        .goods_list {
+display: flex;
+flex-wrap: wrap;
+          .goods-item {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+            width: 33.33%;
+
+            .goods-img {
+              width: 120rpx;
+              height: 120rpx;
+            }
+            .text {
+              font-size: 22rpx;
+            }
+          }
+        }
+      }
     }
   }
 }
