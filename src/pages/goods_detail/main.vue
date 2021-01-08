b593a30d4f6f72a5c3508177599f1788878cd954
weixiaoshan@qq.com <weixiaoshan@qq.com>
2021-01-08 12:19:35 +0800

    商品详情页的数据获取

--- src/pages/goods_detail/main.vue
+++ src/pages/goods_detail/main.vue
@@ -1,9 +1,36 @@
 <template>
-  <view>授权</view>
+  <view>商品详情{{goods_id}}</view>
 </template>
 
 <script>
 export default {
+  data(){
+    return{
+goods_id:'',
+detailData:[]
+  }},
+
+onLoad({goods_id}){
+   this.goods_id=goods_id
+  this.getDetail()
+ 
+
+},
+methods:{
+  getDetail(){
+    uni.request({
+      url: 'https://api-hmugo-web.itheima.net/api/public/v1/goods/detail', 
+      data: {
+    goods_id:this.goods_id
+      },
+      success: (res) => {
+        console.log(res.data);
+   
+      }
+    });
+
+  }
+}
 
 }
 </script>
