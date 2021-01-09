b13ecb6b05bfa2a785c96f04fbd12b5d584af808
weixiaoshan@qq.com <weixiaoshan@qq.com>
2021-01-09 14:58:39 +0800

    注释

--- src/utils/request.js
+++ src/utils/request.js
@@ -3,7 +3,7 @@ const baceUrl ="https://api-hmugo-web.itheima.net/api/public/v1"
 export const request=((params)=>{
     return new Promise((resolve,reject)=>{
         uni.request({
-            ...params,
+            ...params, //...展开运算符，这里当params接收到传入的数据是，比如data，methoddeng，会自动接收
             url:baceUrl + params.url,  
             success: (res) => {
                 resolve(res)
