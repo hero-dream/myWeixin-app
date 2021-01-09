288a096861427ce1e9f06fe70a97286dd6a4ac12
weixiaoshan@qq.com <weixiaoshan@qq.com>
2021-01-09 16:59:07 +0800

    轮廓图的完成

--- src/pages/goods_detail/main.vue
+++ src/pages/goods_detail/main.vue
@@ -1,40 +1,56 @@
 <template>
-  <view>商品详情{{goods_id}}</view>
+  <view class="content">
+
+    <view class="content_in" >
+      <swiper class="swiper" circular :indicator-dots="indicatorDots" indicator-active-color="#2dc2da" >
+        <swiper-item  v-for="item in pics" :key="item.pics_id">
+   <image class="item_image" :src="item.pics_big_url" mode="aspectFit"/>
+        </swiper-item>
+      </swiper>
+      <view class="name">
+
+      </view>
+    </view>
+  </view>
 </template>
 
 <script>
+import {getDetail} from "@/api"
 export default {
-  data(){
-    return{
-goods_id:'',
-detailData:[]
-  }},
+  data() {
+    return {
+     indicatorDots:true,
+      goods_id: "",
+      pics: [],
+    };
+  },
 
-onLoad({goods_id}){
-   this.goods_id=goods_id
-  this.getDetail()
- 
-
-},
-methods:{
-  getDetail(){
-    uni.request({
-      url: 'https://api-hmugo-web.itheima.net/api/public/v1/goods/detail', 
-      data: {
-    goods_id:this.goods_id
-      },
-      success: (res) => {
-        console.log(res.data);
-   
-      }
-    });
-
-  }
-}
-
-}
+  onLoad({ goods_id }) {
+    this.goods_id = goods_id;
+    this.getDetail();
+  },
+  methods: {
+async  getDetail() {
+const res =await getDetail({goods_id: this.goods_id})
+const { pics} =res.data.message
+this.pics=pics
+ console.log(res.data);
+ console.log(pics);
+    },
+  },
+};
 </script>
 
-<style>
+<style lang="less" scoped>
+.content {
+  .content_in {
+display: flex;
+  .swiper,.item_image{
+    width: 750rpx;
+    height: 500rpx;
 
+  }
+    
+  }
+}
 </style>
