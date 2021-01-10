55a41dcfe5eb66168db002d4d571ff9098cd750f
weixiaoshan@qq.com <weixiaoshan@qq.com>
2021-01-10 20:36:25 +0800

    对购物车的初步数据渲染

--- src/pages/cart/main.vue
+++ src/pages/cart/main.vue
@@ -1,13 +1,31 @@
 <template>
-  <view>购物车</view>
+  <view>
+  
+    
+<view>
+<GoodItem v-for="item in cartList" :key="item.goods_id" :item="item"></GoodItem>
+</view>
+  </view>
 </template>
 
 <script>
+import GoodItem from "@/components/goodItem"
+
 export default {
+  components:{GoodItem},
+  data(){return{
+    cartList:''
+  }},
+  onShow(){
+    // 因为tabBar只加载一次，用onlade不合适
+  this.cartList=  uni.getStorageSync('cartList')
+
+  }
 
 }
 </script>
 
-<style>
+<style lang="less">
+
 
 </style>
