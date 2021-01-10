aab620e74a0853eee5b569e898b6bd01113a57c9
weixiaoshan@qq.com <weixiaoshan@qq.com>
2021-01-10 14:48:29 +0800

    对齐代码

--- src/pages/goods_detail/main.vue
+++ src/pages/goods_detail/main.vue
@@ -21,14 +21,13 @@
         </swiper-item>
       </swiper>
     </view>
-<view class="prices">
-    <view class="goods_price">
-      {{ goods_price }}
-    </view>
- 
-    <view class="iconfont icon-zhuanfa"></view>
-    <view class="iconfont icon-shoucang"></view>
+    <view class="prices">
+      <view class="goods_price">
+        {{ goods_price }}
+      </view>
 
+      <view class="iconfont icon-zhuanfa"></view>
+      <view class="iconfont icon-shoucang"></view>
     </view>
     <view class="goods_name">
       {{ goods_name }}
@@ -41,8 +40,12 @@
     <view class="bottom">
       <view class="buttonList">
         <view class="iconfonts">
-      <view class="iconfont icon-kefu"><text class="text">联系客服</text></view>
-        <view class="iconfont icon-gouwuche1"><text class="text">购物车</text></view>
+          <view class="iconfont icon-kefu"
+            ><text class="text">联系客服</text></view
+          >
+          <view class="iconfont icon-gouwuche1"
+            ><text class="text">购物车</text></view
+          >
         </view>
         <button class="cat">加入购物车</button>
         <button class="buy">立即购买</button>
@@ -72,20 +75,20 @@ export default {
   methods: {
     async getDetail() {
       const res = await getDetail({ goods_id: this.goods_id });
-     let {
-       //const为常量，不能重新赋值，let可以
+      let {
+        //const为常量，不能重新赋值，let可以
         pics,
         goods_price,
         goods_introduce,
         goods_name,
       } = res.data.message;
       this.pics = pics;
- const {system} = uni.getSystemInfoSync(); //获取版本信息
- console.log(system);
- const isIos =system.toLowerCase().includes('ios') //toLowerCase将字母转为小写，includes是否包含
-if(isIos){
-  goods_introduce =goods_introduce.replace(/webp/g,'jpg') //当机型为ios时webp转为jpg，因为ios系统不支持webp模式
-}
+      const { system } = uni.getSystemInfoSync(); //获取版本信息
+      console.log(system);
+      const isIos = system.toLowerCase().includes("ios"); //toLowerCase将字母转为小写，includes是否包含
+      if (isIos) {
+        goods_introduce = goods_introduce.replace(/webp/g, "jpg"); //当机型为ios时webp转为jpg，因为ios系统不支持webp模式
+      }
       this.goods_price = goods_price;
       // replace() 方法用于在字符串中用一些字符替换另一些字符，或替换一个与正则表达式匹配的子串
       this.goods_introduce = goods_introduce.replace(
@@ -118,35 +121,33 @@ if(isIos){
       height: 750rpx;
     }
   }
-  .prices{
-display: flex;
-  width: 50rpx;
+  .prices {
+    display: flex;
+    width: 50rpx;
     height: 100rpx;
     line-height: 100rpx;
-  .goods_price {
-  
-    color: red;
-    font-weight: 700;
-    font-size: 36rpx;
-    padding: 0 490rpx 0 20rpx;
-    &::before {
-      content: "￥";
-      font-size: 25rpx;
+    .goods_price {
       color: red;
+      font-weight: 700;
+      font-size: 36rpx;
+      padding: 0 490rpx 0 20rpx;
+      &::before {
+        content: "￥";
+        font-size: 25rpx;
+        color: red;
+      }
     }
-    }
-  
-    .icon-shoucang
-   {
+
+    .icon-shoucang {
       font-size: 50rpx;
       color: #707070;
-      padding: 0 20rpx ;
+      padding: 0 20rpx;
     }
-     .icon-zhuanfa{
-        font-size: 40rpx;
+    .icon-zhuanfa {
+      font-size: 40rpx;
       color: #707070;
-      padding: 0 20rpx ;
-      }
+      padding: 0 20rpx;
+    }
   }
   .goods_name {
     padding: 0 20rpx 20rpx 20rpx;
@@ -180,22 +181,21 @@ display: flex;
       justify-content: space-evenly;
       padding: 20rpx 0;
     }
-.iconfonts{
-display: flex;
-justify-content: space-evenly;
-.icon-kefu,
-.icon-gouwuche1
-{
-  padding: 0 30rpx;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+    .iconfonts {
+      display: flex;
+      justify-content: space-evenly;
+      .icon-kefu,
+      .icon-gouwuche1 {
+        padding: 0 30rpx;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
 
-  .text{
-    font-size: 23rpx;
-  }
-}
-}
+        .text {
+          font-size: 23rpx;
+        }
+      }
+    }
     .cat {
       width: 196rpx;
       height: 60rpx;
