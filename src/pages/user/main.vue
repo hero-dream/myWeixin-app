1c5716d9449175bf85e253874ddee545fc65e769
weixiaoshan@qq.com <weixiaoshan@qq.com>
2021-01-13 16:32:42 +0800

    个人中心的按钮布局

--- src/pages/user/main.vue
+++ src/pages/user/main.vue
@@ -1,13 +1,31 @@
 <template>
-  <view>个人中心页面</view>
+  <view>
+    <view class="user_head">
+      <button class="button" type="warn">请登录</button>
+    </view>
+  </view>
 </template>
 
 <script>
 export default {
+ 
 
 }
 </script>
 
-<style>
+<style lang="less" scoped>
+.user_head{
+  height: 350rpx;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  .button{
+    width: 352rpx;
+    height: 88rpx;
+    color: #fff;
+    font-size: 36rpx;
+    
+  }
 
+}
 </style>
