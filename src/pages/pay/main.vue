95c75791ad7a86092a0b5f8a6dae12ede2275dba
weixiaoshan@qq.com <weixiaoshan@qq.com>
2021-01-14 11:08:36 +0800

    头部按钮获取地址的布局

--- src/pages/pay/main.vue
+++ src/pages/pay/main.vue
@@ -1,5 +1,11 @@
 <template>
-  <view>结算</view>
+  <view>
+    <view class="contenr">
+    <view class="pay_button">
+      <button class="button"><text class="text">添加地址</text></button>
+    </view>
+</view>
+  </view>
 </template>
 
 <script>
@@ -8,6 +14,23 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="less">
+.contenr{
+.pay_button {
+  height: 350rpx;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #f9f9f9;
+  .button {
+    width: 352rpx;
+    height: 88rpx;
+    color: red;
+    border:red solid 1rpx;
+    font-size: 36rpx;
+    border-radius: 44rpx;
+  }
+  }
+}
 </style>
