73bdf4e831a26a0ec6ae4484f4a9c6f934749306
weixiaoshan@qq.com <weixiaoshan@qq.com>
2021-01-15 16:42:07 +0800

    客服和转发的api功能页面布局的全局引入

--- src/App.vue
+++ src/App.vue
@@ -14,4 +14,16 @@
 <style lang="less">
 @import "/style/icon.css";
 // 每个页面公共css 
+     .open_type_wrap {
+      position: relative;
+  .open_type_button {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    left: 10;
+	top: 0;
+	opacity: 0;
+
+  }
+}
 </style>
