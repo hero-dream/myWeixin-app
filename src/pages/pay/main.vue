2a5f1fa687e859d235bf404d4af7b1f1f19714d6
weixiaoshan@qq.com <weixiaoshan@qq.com>
2021-01-15 16:07:07 +0800

    支付页面的商品列表数据渲染完成和底部的布局和数据渲染也ok

--- src/pages/pay/main.vue
+++ src/pages/pay/main.vue
@@ -14,15 +14,35 @@
       <view class="pay_button" v-else @tap="chooseAddress">
         <button class="button"><text class="text">添加地址</text></button>
       </view>
+      <view class="shopList">
+        <view class="text">已选商品</view>
+        <view class="list">
+          <GoodItem v-for="item in cartList" :key="item.goods_id" :item="item"></GoodItem>
+        </view>
+      </view>
+    </view>
+    <view class="bottom">
+      <view class="bottom-left">
+        <view class="all">合计:</view>
+        <view class="price">{{ allArice }}元</view>
+      </view>
+      <view class="bottom-right">
+        <navigator hover-class="none" url="/pages/pay/main" class="count"
+          >去支付({{ allCount }})</navigator
+        >
+      </view>
     </view>
   </view>
 </template>
 
 <script>
+import GoodItem from "@/components/goodItem";
 export default {
+  components:{GoodItem},
   data() {
     return {
       address: {},
+      cartList:[]
     };
   },
   computed: {
@@ -30,7 +50,29 @@ export default {
       const { provinceName, cityName, countyName, detailInfo } = this.address;
       return provinceName + cityName + countyName + detailInfo;
     },
-  },
+
+
+   allArice() {
+      let allArice = 0;
+      this.cartList.forEach((item) => {
+        if (item.goods_selected) {
+          allArice += item.goods_price * item.goods_count;
+        }
+      });
+      return allArice;
+    },
+
+  allCount() {
+      let allcount = 0;
+      this.cartList.forEach((item) => {
+        if (item.goods_selected) {
+          allcount += item.goods_count;
+        }
+      });
+      return allcount;
+    },
+},
+  
 
   methods: {
     chooseAddress() {
@@ -46,6 +88,10 @@ export default {
   onLoad() {
     const address = uni.getStorageSync("address");
     this.address = address;
+
+  this.cartList= uni.getStorageSync('cartList') || []
+
+
   },
 };
 </script>
@@ -89,6 +135,71 @@ export default {
       .name {
         padding-right: 40rpx;
       }
+    }
+  }
+  .shopList {
+    margin: 0 20rpx 90rpx 20rpx;
+    width: 711rpx;
+    min-height: 489rpx;
+    background-color: #f9f9f9;
+  .text {
+padding: 17rpx;
+font-size: 26rpx;
+  }
+
+  .list {
+    
+  border-bottom: 1rpx solid red;
+  }
+}
+
+}
+.bottom {
+  width: 100%;
+  height: 83rpx;
+  background-color: #fff;
+  border-top: #ccc solid 1rpx;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  z-index: 999;
+  display: flex;
+  align-items: center;
+
+  .bottom-left {
+    flex: 1;
+    display: flex;
+    // justify-content: center;
+    align-items: center;
+    .content_left_radio {
+      padding: 0 20rpx 0 30rpx;
+    }
+    .all {
+      font-size: 26rpx;
+      padding: 0 20rpx;
+    }
+    .price {
+      color: red;
+      font-weight: 700;
+      &::before {
+        content: "￥";
+        font-size: 25rpx;
+        color: red;
+      }
+    }
+  }
+
+  .bottom-right {
+    padding: 0 30rpx 0 0;
+    .count {
+      color: #fff;
+      font-size: 21rpx;
+      line-height: 52rpx;
+      text-align: center;
+      width: 150rpx;
+      height: 52rpx;
+      background-color: red;
+      border-radius: 52rpx;
     }
   }
 }
